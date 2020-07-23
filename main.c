09d57e7fc90e4969b3abe488fdc7e648204a27c4
thegenghiskahn <51738321+thegenghiskahn@users.noreply.github.com>
2020-07-23 15:34:31 -0500

    initial commit of blank files to be used in project

---
+++ main.c
@@ -0,0 +1,6 @@
+/*
+* Author: Bennett Kahn
+* Date Began: 7/23/20
+* Program Description: A simple matrix calculator that can perform all basic Linear Algebra functions
+	* The purpose of this project is to gain some experience with C and GitHub
+*/
